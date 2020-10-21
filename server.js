301565551b5b0059b095738a6a35285a7995aac2
XC3000 <richardrozario.rr@gmail.com>
2020-10-22 01:26:27 +0530

    middleware

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const logger = require("./middleware/logger");
+const morgan = require("morgan");
 
 // Route files
 const bootcamps = require("./routes/bootcamps");
@@ -11,6 +13,12 @@ dotenv.config({
 
 const app = express();
 
+/* app.use(logger); */
+
+// Dev Logging Middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 // Mount Routers
 app.use("/api/v1/bootcamps", bootcamps);
 
